b0d0bd790c102ebab796b3583084107db0851aba
quaraami <shum05052000@gmail.com>
2025-02-07 13:31:18 +0300

    commit

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,16 @@
 class Cars:
-    mark = 'Lada'
-    tip = 'Cedan'
-    country = 'Russia'
-    def name_country(self, country):
-        print("1")
-cr = Cars()
-cr.name_country("Japan")
-print(cr.__dict__)
-printe)
+    def __init__ (self, name):
+        self.name = name
+class Russian(Cars):
+     def __init__ (self, name):
+        super().__init__(name)
+class Tip(Russian):
+     def __init__ (self, tip):
+        super().__init__(tip)
+class Colour(Tip):
+    def __init__ (self, colour):
+        super().__init__(colour)
+tp = Tip("Vesta")
+ru = Russian("Lada")
+cl = Colour("Red")
+print(ru.__dict__ , tp.__dict__, cl.__dict__)
