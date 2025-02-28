28c7c20e8534982964181cf2f14fda3fdf36e759
quaraami <shum05052000@gmail.com>
2025-02-28 12:41:00 +0300

    commit

---
+++ lesson4.py
@@ -0,0 +1,23 @@
+print("Задание №1")
+class Person:
+    def __init__(self, name, age):
+            self.name = name  
+            self.age = age
+    def __setattr__(self, key, value):
+        if key == 'name':
+            if not isinstance(value, str) or len(value) == 0:
+                raise ValueError("Имя не может быть пустым.")
+        elif key == 'age':
+            if not isinstance(value, (int, float)) or value <= 0:
+                raise ValueError("Возраст должен быть положительным числом.")
+        super().__setattr__(key, value)
+
+person = Person("John", 25)
+print(f"Имя: { person.name}, Возраст: {person.age}")
+p = Person("John", 25)  
+p.name = "Alice"         
+p.age = 30
+print(f"Имя: { p.name}, Возраст: {p.age}")
+p2 = Person("Timofey", 18)  
+p2.name = ""              
+p2.age = -5
