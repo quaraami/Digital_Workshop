6c6413018b05c683ac92ff63a4f928f12c9af053
quaraami <shum05052000@gmail.com>
2025-03-27 15:22:16 +0300

    commit

--- lesson3.py
+++ lesson3.py
@@ -34,3 +34,67 @@ if __name__ == "__main__":
     acc.deposit(500)
     acc.withdraw(200)
     print(acc.get_balance())  # Вывод: 300
+
+print("Задание №2")
+class User:
+        def __init__(self, username, password=None):
+            self.__username = username
+            self.__password = None  # Пароль сначала пустой
+            if password is not None and self.is_strong_password(password):
+                self.__password = password
+        @staticmethod
+        def is_strong_password(password):
+             return len(password) >= 6
+        @classmethod
+        def create_default_user(cls, username):
+             default_password = f"{username}_strong_pass"
+             return cls(username, default_password)
+        def set_password(self, new_password):
+            if self.is_strong_password(new_password):
+                 self.__password = new_password
+                 print("Пароль успешно изменён.")
+            else:
+                 print("Ошибка: пароль слишком короткий.")
+        def get_username(self):
+            return self.__username
+# Пример использования
+if __name__ == "__main__":
+    user = User.create_default_user("Alice")
+    print(user.get_username())  # Вывод: Alice
+    user.set_password("12345")  # Ошибка: пароль слишком короткий
+    user.set_password("securePass")  # Пароль успешно изменён
+
+print("Задание №3")
+import datetime
+
+class Book:
+    def __init__(self, title, author, year):
+        self.__title = title
+        self.__author = author
+        self.__year = self.validate_year(year)
+
+    @staticmethod
+    def validate_year(year):
+        """Проверяет, что год издания корректный (int и не в будущем)."""
+        current_year = datetime.datetime.now().year
+        if isinstance(year, int) and year <= current_year:
+            return year
+        raise ValueError(f"Год {year} должен быть целым числом и не превышать текущий год ({current_year}).")
+
+    @classmethod
+    def create_default_year(cls, title, author):
+        """Создает книгу с годом издания по умолчанию (2024)."""
+        return cls(title, author, 2024)
+
+    def get_info(self):
+        """Возвращает информацию о книге в формате строки."""
+        return f'"{self.__title}", автор: {self.__author}, год: {self.__year}'
+
+
+# Пример использования
+if __name__ == "__main__":
+    book1 = Book("1984", "George Orwell", 1949)
+    print(book1.get_info())  # Вывод: "1984, автор: George Orwell, год: 1949"
+
+    book2 = Book.create_default_year("Brave New World", "Aldous Huxley")
+    print(book2.get_info())  # Вывод: "Brave New World, автор: Aldous Huxley, год: 2024"
--- lesson6.py
+++ lesson6.py
@@ -1,36 +1,60 @@
-import matplotlib.pyplot as plt
+import math
 import numpy as np
-class Derivate:
-    def __init__(self, func, h=1e-5):
-        self.func = func
-        self.h = h
-        self.instance = None
+import matplotlib.pyplot as plt
 
-    def __call__(self, x):
-        return (self.func(x + self.h) - self.func(x - self.h)) / (2 * self.h)
+class Derivative:
+    """Дескриптор для вычисления производной функции"""
+    
+    def __init__(self, func):
+        """ Инициализация дескриптора :param func:param функция, для которой будет вычисляться производная """
+        self.func = func
+
+    def __get__(self, instance, owner):
+        """ Возвращаем либо сам дескриптор, либо функцию для вычисления производной, в зависимости от контекста вызова :param instance: экземпляр класса, владеющего данным дескриптором :param owner: класс, владеющий данным дескриптором :return: дескриптор или функцию для вычисления производной """
+        if instance is None:
+            return self
+        return lambda x: self.__call__(instance, x)
+
+    def __call__(self, instance, x):
+        """ Вычисление производной функции в точке x численным методом :param instance: экземпляр класса, владеющего данным дескриптором :param x: точка, в которой нужно вычислить производную :return: приближенное значение производной """
+        h = 1e-5  # малое смещение для численной аппроксимации
+        return (instance(x + h) - instance(x - h)) / (2 * h)
+
 
 class ExponentialFunction:
+    """Класс для представления функции вида f(x) = a * e^x"""
+    
     def __init__(self, a):
+        """ Инициализация объекта функции :param a: коэффициент перед экспонентой """
         self.a = a
+        self.derivative = Derivative(self)
 
     def __call__(self, x):
-        return self.a * (np.exp ** x)
+        """ Вызов объекта функции как обычной функции :param x: аргумент функции :return: значение функции в точке x """
+        return self.a * math.exp(x)
+
+    def plot(self, start=-2, end=2, num_points=200):
+        """ Построение графиков функции и её производной :param start: начальная точка интервала :param end: конечная точка интервала :param num_points: количество точек для графика """
+        x_values = np.linspace(start, end, num_points)
+        y_values = [self(x) for x in x_values]
+        dy_values = [self.derivative(x) for x in x_values]
+
+        plt.figure(figsize=(8, 6))
+        plt.plot(x_values, y_values, label=r'$f(x)$', color='blue')
+        plt.plot(x_values, dy_values, label=r"$f'(x)$", linestyle='--', color='red')
+        plt.xlabel(r'$x$')
+        plt.ylabel(r'$y$')
+        plt.title(f'График функции $f(x)={self.a}e^{{{x}}}$ и её производной')
+        plt.grid(True)
+        plt.legend(loc='best')
+        plt.show()
 
 
-exp_func = ExponentialFunction(a=1)
+# Пример использования
+if __name__ == "__main__":
+    exp_func = ExponentialFunction(a=2)
+    print("Значение функции в точке x=0:", exp_func(0))       # Должно вывести 2.0
+    print("Производная функции в точке x=0:", exp_func.derivative(0))  # Должно вывести 2.0
 
-x_values = np.linspace(-2, 2, 1000)
-
-y_values = exp_func(x_values)
-dy_values = exp_func.instance(x_values)
-
-# Строим графики
-plt.figure(figsize=(8, 6))
-plt.plot(x_values, y_values, label='f(x)')
-plt.plot(x_values, dy_values, label="f'(x)")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('График функции и её производной')
-plt.legend()
-plt.grid(True)
-plt.show()
+    # Построение графиков
+    exp_func.plot()
