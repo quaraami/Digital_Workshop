46458cd9f398b0b452ba6fa34854f71854f93443
quaraami <shum05052000@gmail.com>
2025-03-28 12:32:07 +0300

    commit

---
+++ kr1.py
@@ -0,0 +1,62 @@
+#Задание №4
+import math
+
+class Vector:
+    def __init__(self, *args):
+        for coord in args:
+            if not isinstance(coord, (int, float)):
+                raise TypeError("Все координаты вектора должны быть числами.")
+        self.coords = list(args)
+
+    def __len__(self): #кол-во координат
+        return len(self.coords)
+
+    def __abs__(self): #евклидова норма
+        sum_of_squares = sum(coord ** 2 for coord in self.coords)
+        return math.sqrt(sum_of_squares)
+    
+
+v = Vector(1, 2, 3)
+print(len(v))      #3
+print(abs(v))      #3.7416573867739413
+
+v = Vector(0, -5)
+print(len(v))      # 2
+print(abs(v))      # 5.0
+
+try:
+    v = Vector("a", 2)
+except TypeError as e:
+    print(e)       # TypeError
+
+#Задание №3
+class CacheCalculator:
+    def __init__(self):
+        self._cache = {}
+    
+    def __call__(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError(f"Аргумент должен быть int, а не {type(x).__name__}")
+        
+        if x in self._cache:
+            return self._cache[x]
+        
+        result = x ** 2
+        self._cache[x] = result
+        return result
+
+    @property
+    def cache(self):
+        return self._cache
+calc = CacheCalculator()
+result_1 = calc(2)
+print(result_1)      # Выводит: 4
+print(calc.cache)    # Выводит: {2: 4}
+result_2 = calc(2.5)
+print(result_2)      # Выводит: 6.25
+print(calc.cache)    # Выводит: {2: 4, 2.5: 6.25}
+
+try:
+    calc("text")
+except TypeError as error:
+    print(error)         # Выводит: TypeError
--- lesson6.py
+++ lesson6.py
@@ -1,60 +1,38 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Derivative:
-    """Дескриптор для вычисления производной функции"""
-    
-    def __init__(self, func):
-        """ Инициализация дескриптора :param func:param функция, для которой будет вычисляться производная """
+    def __init__(self, h=10**(-5)):
+        self.func=None
+        self.h = h
+    def __get__(self, func, owner):
         self.func = func
-
-    def __get__(self, instance, owner):
-        """ Возвращаем либо сам дескриптор, либо функцию для вычисления производной, в зависимости от контекста вызова :param instance: экземпляр класса, владеющего данным дескриптором :param owner: класс, владеющий данным дескриптором :return: дескриптор или функцию для вычисления производной """
-        if instance is None:
-            return self
-        return lambda x: self.__call__(instance, x)
-
-    def __call__(self, instance, x):
-        """ Вычисление производной функции в точке x численным методом :param instance: экземпляр класса, владеющего данным дескриптором :param x: точка, в которой нужно вычислить производную :return: приближенное значение производной """
-        h = 1e-5  # малое смещение для численной аппроксимации
-        return (instance(x + h) - instance(x - h)) / (2 * h)
-
+        return self
+    def __call__(self, x):
+        return (self.func(x + self.h) - self.func(x - self.h)) / (2 * self.h)
 
 class ExponentialFunction:
-    """Класс для представления функции вида f(x) = a * e^x"""
-    
+    derivative = Derivative(10**(-5))
     def __init__(self, a):
-        """ Инициализация объекта функции :param a: коэффициент перед экспонентой """
         self.a = a
-        self.derivative = Derivative(self)
-
     def __call__(self, x):
-        """ Вызов объекта функции как обычной функции :param x: аргумент функции :return: значение функции в точке x """
-        return self.a * math.exp(x)
-
-    def plot(self, start=-2, end=2, num_points=200):
-        """ Построение графиков функции и её производной :param start: начальная точка интервала :param end: конечная точка интервала :param num_points: количество точек для графика """
-        x_values = np.linspace(start, end, num_points)
-        y_values = [self(x) for x in x_values]
-        dy_values = [self.derivative(x) for x in x_values]
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(x_values, y_values, label=r'$f(x)$', color='blue')
-        plt.plot(x_values, dy_values, label=r"$f'(x)$", linestyle='--', color='red')
-        plt.xlabel(r'$x$')
-        plt.ylabel(r'$y$')
-        plt.title(f'График функции $f(x)={self.a}e^{{{x}}}$ и её производной')
-        plt.grid(True)
-        plt.legend(loc='best')
+        return self.a * np.exp(x)
+    def plot(self):
+        x = np.linspace(-2, 2, 500)
+        y = self(x)
+        dy = self.derivative(x)
+        
+        plt.figure(figsize=(10, 6))
+        plt.plot(x, y, label='f(x) = a * e^x', color='red')
+        plt.plot(x, dy, label="f'(x)", linestyle='dashed', color='blue')
+        plt.legend()
+        plt.xlabel('x')
+        plt.ylabel("f(x) и f'(x)")
+        plt.title('Графики функции и её производной')
+        plt.grid()
         plt.show()
 
-
-# Пример использования
-if __name__ == "__main__":
-    exp_func = ExponentialFunction(a=2)
-    print("Значение функции в точке x=0:", exp_func(0))       # Должно вывести 2.0
-    print("Производная функции в точке x=0:", exp_func.derivative(0))  # Должно вывести 2.0
-
-    # Построение графиков
-    exp_func.plot()
+exp_func = ExponentialFunction(a=2)
+print(exp_func(0))         
+print(exp_func.derivative(0))
+exp_func.plot()
